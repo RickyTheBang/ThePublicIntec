fed844d4ae8ad6d3a3cb264786ff8d02cb1800f4
Eric Fagnoul <eric.fagnoul@skynet.be>
2019-05-02 15:46:13 +0200

    v02

--- FileIO_Opdracht/src/be/intecbrussel/IOApp.java
+++ FileIO_Opdracht/src/be/intecbrussel/IOApp.java
@@ -1,5 +1,6 @@
 package be.intecbrussel;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
@@ -7,83 +8,106 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class IOApp {
 
 
+    public static void main(String[] args) {
 
 
-        public static void main(String[] args) {
+        // try {
+
+        Path path = Paths.get ("C:/MyDirectory/Eclipse/Java11Fundamentals/FileIO_Opdracht/src/resources/sortedFolder");
+        Path pathSource = Paths.get ("C:/MyDirectory/Eclipse/Java11Fundamentals/FileIO_Opdracht/src/resources/unsortedFolder");
 
 
-            try {
 
-                Path path = Paths.get("C:/MyDirectory/Eclipse/Java11Fundamentals/FileIO_Opdracht/src/resources/sortedFolder");
 
-                checkIfDirExistElseCreate(path);
 
-                //checkIfFileExistElseCreate(path);
+        File directory = new File (pathSource.toString ());
+        List<File> result =  getFile(directory);
 
-               // writeSomeLinesToFile(path);
-/*
-                Files.lines(path).forEach(System.out::println);
 
-                Files.copy(path, Paths.get("c:/data/folder1/testing2.text"));
-                System.out.println("Copy is created");
+      List<File> folder = result.stream ()
+                .filter(a->a.isDirectory ())
+               .collect (Collectors.toList ());
 
-                String feedback = (Files.deleteIfExists(path)) ? "deleted" : "non existing";
-                System.out.println("The file is: " + feedback);
-*/
-            } catch (IOException e) {
-                System.out.println("Whoops: " + e.getMessage());
-            }
+
+
+
+
+        for(File f : folder){
+            System.out.println (f.getName ());
         }
 
+
+
+
+    }
+
+    //File directory = new File (pathSource.toString ());
+    private static  List<File> getFile(File directory){
+
+
+        List<File> all = new ArrayList<> ();
+
+        File[] fList = directory.listFiles ();
+
+        for (File file : fList) {
+            if (file.isFile ()) {
+               all.add (file);
+                 //System.out.println (file.getName ());
+
+            } else if (file.isDirectory ()) {
+               System.out.println (file.getName ());
+                all.add (file);
+                getFile(file);
+
+            }
+        }
+return all;
+    }
+
+ /*
+ private static void getFile(Path path){
+     File directory = new File (path.toString ());
+     File[] fList = directory.listFiles ();
+
+     for (File file : fList) {
+         if (file.isFile ()) {
+             // System.out.println (file.getName ());
+         } else if (file.isDirectory ()) {
+             System.out.println (file.getName ());
+         }
+     }
+ }
+*/
     private static void checkIfDirExistElseCreate(Path path) throws IOException {
-        if (Files.notExists(path)) {
-            Files.createDirectories(path);
-            System.out.println("Dir is created");
+        if (Files.notExists (path)) {
+            Files.createDirectories (path);
+            System.out.println ("Dir is created");
         } else {
-            System.out.println("Dir arlready exists ");
+            System.out.println ("Dir arlready exists ");
         }
 
 
     }
 
     private static void checkIfFileExistElseCreate(Path path) throws IOException {
-        if (Files.notExists((path))) {
-            Files.createFile(path);
-            System.out.println("Files is created!");
+        if (Files.notExists ((path))) {
+            Files.createFile (path);
+            System.out.println ("Files is created!");
 
         } else {
-            System.out.println("File already exists.");
+            System.out.println ("File already exists.");
         }
     }
 
-        private static void writeSomeLinesToFile(Path path) throws IOException {
-            if (path != null) {
-                List<String> lines = new ArrayList<> ();
-
-                lines.add("Hello");
-                lines.add("World");
-                lines.add("how");
-                lines.add("do");
-                lines.add("you");
-                lines.add("do");
-                lines.add("?");
-                lines.add("?");
-
-                Files.write(path, lines, Charset.defaultCharset(), StandardOpenOption.APPEND);
-
-
-            }
-        }
 
 
 
-
-
-
-      }
+}
 
