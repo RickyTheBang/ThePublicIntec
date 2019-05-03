783ad4a54841c6796d6933a239c5a7a5f1835a30
Eric Fagnoul <eric.fagnoul@skynet.be>
2019-05-03 12:01:07 +0200

    v03

--- FileIO_Opdracht/src/be/intecbrussel/app/IOApp.java
+++ FileIO_Opdracht/src/be/intecbrussel/app/IOApp.java
@@ -1,5 +1,6 @@
 package be.intecbrussel.app;
 
+import be.intecbrussel.entities.FileAttributes;
 import be.intecbrussel.utilities.FilesRepository;
 import be.intecbrussel.utilities.PathTools;
 
@@ -13,96 +14,88 @@ import java.util.stream.Collectors;
 
 public class IOApp {
 
-    public static List<File> all = new ArrayList<>();
+    public static List<File> all = new ArrayList<> ();
 
     public static void main(String[] args) {
 
 
         try {
 
-            Path sortedPath = Paths.get("C:/data/sorted");
-            Path unsortedPath = Paths.get("C:/data/unsorted");
-            PathTools.checkIfDirExistElseCreate(sortedPath);
+            Path sortedPath = Paths.get ("C:/data/sorted");
+            Path unsortedPath = Paths.get ("C:/data/unsorted");
+            PathTools.checkIfDirExistElseCreate (sortedPath);
 
-            File directory = new File(unsortedPath.toString());
+            File directory = new File (unsortedPath.toString ());
 
-            FilesRepository fr = new FilesRepository();
+            FilesRepository fr = new FilesRepository ();
 
-            List<File> result = fr.fillFileAndDir(directory);
-
-
-            List<File> folder = result.stream()
-                    .filter(a -> a.isDirectory())
-                    .collect(Collectors.toList());
-
-            List<File> document = result.stream()
-                    .filter(a -> a.isFile())
-                    .collect(Collectors.toList());
-
-File test = document.get (1);
-            System.out.println (test);
-            System.out.println (test.getName ());
-            System.out.println (test.canRead ());
-            System.out.println (test.canWrite ());
-            System.out.println (test.getParent ());
-            System.out.println (test.getParentFile ());
-            System.out.println (test.getAbsoluteFile ());
-
-            Path p20 = test.getParentFile ().toPath ().resolve (test.getName ());
-            System.out.println (p20);
+            List<File> result = fr.fillFileAndDir (directory);
 
 
+            List<File> folder = result.stream ()
+                    .filter (a -> a.isDirectory ())
+                    .collect (Collectors.toList ());
 
-            for (File f : document) {
-                 System.out.println(f);
+            List<File> document = result.stream ()
+                    .filter (a -> a.isFile ())
+                    .collect (Collectors.toList ());
 
 
-            }
-
-            Set<String> extension = new HashSet<>();
-            Map<Path,String> mapFileDir = new HashMap<>();
+            Set<String> extension = new HashSet<> ();
+            Map<File, String> mapFileDir = new HashMap<> ();
             int count1 = 0;
 
             for (File d : document) {
-                System.out.println(d.getName());
-                String type = PathTools.getSubString(d.getName());
-                extension.add(type);
+                //  System.out.println(d.getName());
+                String type = PathTools.getSubString (d.getName ());
+                extension.add (type);
                 count1++;
 
 
-                mapFileDir.put(d.toPath(),type);
+                mapFileDir.put (d, type);
                 //Path p5 = d.toPath().resolve(d.getName());
-               // PathTools.checkIfFileExistElseCreate(p5);
+                // PathTools.checkIfFileExistElseCreate(p5);
                 //System.out.println(type);
             }
 
 
-
-            System.out.println("*****************************");
+            System.out.println ("*****************************");
 
             int count2 = 0;
 
-            for(Path f: mapFileDir.keySet()){
+            List<FileAttributes> listFileAttributes = new ArrayList<> ();
+
+            for (File test : mapFileDir.keySet ()) {
                 count2++;
-              System.out.println(f.getFileName()+ ": " + mapFileDir.get(f));
-                //Path path10 = f.resolve(mapFileDir.get(f));
-               // System.out.println(path10);
-             //  PathTools.checkIfFileExistElseCreate(path10);
+                FileAttributes fa = new FileAttributes (test, mapFileDir.get (test));
+                listFileAttributes.add (fa);
+
             }
+
+            for (FileAttributes fa : listFileAttributes) {
+                PathTools.checkIfFileExistElseCreate (fa.getCompletePath ());
+            }
+
+            String dir = listFileAttributes.get (2).getDirectory ();
+            File fileO = listFileAttributes.get (2).getFileName ();
+            System.out.println (dir);
+            System.out.println (fileO);
+
+            PathTools.checkIfFileExistElseCreate (listFileAttributes.get (2).getCompletePath ());
 
             System.out.println ("1:" + count1);
             System.out.println ("2:" + count2);
-/*
+
             for (String s : extension) {
 
-                Path p2 = sortedPath.resolve(s);
-                PathTools.checkIfDirExistElseCreate(p2);
+                Path p2 = sortedPath.resolve (s);
+                // PathTools.checkIfDirExistElseCreate(p2);
 
             }
-*/
+
 
         } catch (IOException e) {
-            System.out.println("Whoops: " + e.getMessage());
+            System.out.println ("Whoops: " + e.getMessage ());
         }
     }
 
--- FileIO_Opdracht/src/be/intecbrussel/entities/FileAttributes.java
+++ FileIO_Opdracht/src/be/intecbrussel/entities/FileAttributes.java
@@ -1,13 +1,83 @@
 package be.intecbrussel.entities;
 
+import java.io.File;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
 
 public class FileAttributes {
 
-    Path path;
-    String directory;
-    boolean writable;
-    boolean readable;
+
+    private Path sortedDirectory = Paths.get ("C:\\data\\sorted");
+    private Path CompleteDirectory;
+    private Path CompletePath;
+    private String directory;
+    private File fileName;
+    private boolean writable;
+    private boolean readable;
+
+    public FileAttributes(File file, String directory) {
+
+        setDirectory (directory);
 
 
+        setReadable (file.canRead ());
+        setWritable (file.canWrite ());
+
+        setCompleteDirectory (sortedDirectory.resolve (directory));
+        Path path21 = file.toPath ().getFileName ();
+        setCompletePath (getCompleteDirectory ().resolve (file.toPath ().getFileName ()));
+        setFileName (file.toPath ().getFileName ().toFile ());
+
+
+    }
+
+
+    public Path getCompleteDirectory() {
+        return CompleteDirectory;
+    }
+
+    public void setCompleteDirectory(Path completeDirectory) {
+        CompleteDirectory = completeDirectory;
+    }
+
+    public Path getCompletePath() {
+        return CompletePath;
+    }
+
+    public void setCompletePath(Path completePath) {
+        CompletePath = completePath;
+    }
+
+    public String getDirectory() {
+        return directory;
+    }
+
+    public void setDirectory(String directory) {
+        this.directory = directory;
+    }
+
+    public File getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(File fileName) {
+        this.fileName = fileName;
+    }
+
+    public boolean isWritable() {
+        return writable;
+    }
+
+    public void setWritable(boolean writable) {
+        this.writable = writable;
+    }
+
+    public boolean isReadable() {
+        return readable;
+    }
+
+    public void setReadable(boolean readable) {
+        this.readable = readable;
+    }
 }
